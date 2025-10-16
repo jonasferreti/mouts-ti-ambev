c3bb67317b28bc8d3acaff0e148530f36a68dc6e
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-16 05:56:27 -0300

    feat(application): add NotFoundException

---
+++ src/Ambev.DeveloperEvaluation.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
