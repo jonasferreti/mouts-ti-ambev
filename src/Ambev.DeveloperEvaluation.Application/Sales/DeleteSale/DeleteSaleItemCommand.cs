c96aa257df8ea8572d57b75e1d059b464d6a29ab
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-16 22:06:24 -0300

    feat(application): add DeleteSale command

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleItemCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+/// <summary>
+/// Command to remove a specific SaleItem from a Sale Aggregate Root.
+/// </summary>
+public record DeleteSaleItemCommand(Guid SaleId, Guid ItemId) : IRequest;
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleItemHandler.cs
@@ -0,0 +1,38 @@
+﻿using Ambev.DeveloperEvaluation.Application.Exceptions;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+/// <summary>
+/// Handles item deletion. If the last item is deleted, it performs a cascading deletion 
+/// of the parent Sale Aggregate Root.
+/// </summary>
+public class DeleteSaleItemHandler : IRequestHandler<DeleteSaleItemCommand>
+{
+    private readonly ISaleRepository _saleRepository;
+
+    public DeleteSaleItemHandler(ISaleRepository saleRepository)
+    {
+        _saleRepository = saleRepository;
+    }
+
+    public async Task Handle(DeleteSaleItemCommand command, CancellationToken cancellationToken)
+    {
+        var validator = new DeleteSaleItemValidator();
+        var validationResult = await validator.ValidateAsync(command, cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var sale = await _saleRepository.GetByIdForUpdateAsync(command.SaleId, cancellationToken)
+            ?? throw new NotFoundException($"Sale with ID {command.SaleId} not found.");
+
+        sale.RemoveItem(command.ItemId);
+
+        if (sale.HasItems())
+            await _saleRepository.UpdateAsync(sale, cancellationToken);
+        else
+            await _saleRepository.DeleteAsync(sale, cancellationToken);
+    }
+}
