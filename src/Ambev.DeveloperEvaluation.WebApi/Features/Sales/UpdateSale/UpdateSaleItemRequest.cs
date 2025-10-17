70a528563994f5c0682aed4ac73f507b4cfa5be1
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-17 06:32:11 -0300

    feat(api): add UpdateSale request

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/UpdateSale/UpdateSaleItemRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.UpdateSale;
+
+/// <summary>
+/// API DTO used to receive item data for the update sale endpoint
+/// </summary>
+public class UpdateSaleItemRequest
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/UpdateSale/UpdateSaleRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.UpdateSale;
+
+/// <summary>
+/// API DTO used to receive sale data for the update sale endpoint.
+/// </summary>
+public class UpdateSaleRequest
+{
+    [JsonIgnore]
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public Guid BranchId { get; set; }
+    public string BranchName { get; set; } = string.Empty;  
+
+    public List<UpdateSaleItemRequest> Items { get; set; } = [];
+}
