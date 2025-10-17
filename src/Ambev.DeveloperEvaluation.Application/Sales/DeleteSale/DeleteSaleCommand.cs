314dc97a1e7655fe7ad614610763111819460b9e
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-16 22:06:24 -0300

    feat(application): add DeleteSale command

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+/// <summary>
+/// Command to delete a Sale Aggregate Root.
+/// </summary>
+public record DeleteSaleCommand(Guid Id) : IRequest;
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleHandler.cs
@@ -0,0 +1,26 @@
+﻿using Ambev.DeveloperEvaluation.Application.Exceptions;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+/// <summary>
+/// Handles the DeleteSaleCommand, performing the final deletion of the Aggregate Root.
+/// </summary>
+public class DeleteSaleHandler : IRequestHandler<DeleteSaleCommand>
+{
+    private readonly ISaleRepository _saleRepository;
+
+    public DeleteSaleHandler(ISaleRepository saleRepository)
+    {
+        _saleRepository = saleRepository;
+    }
+
+    public async Task Handle(DeleteSaleCommand command, CancellationToken cancellationToken)
+    {
+        var sale = await _saleRepository.GetByIdAsync(command.Id, cancellationToken)
+            ?? throw new NotFoundException($"Sale with ID {command.Id} not found.");
+
+        await _saleRepository.DeleteAsync(sale, cancellationToken);
+    }
+}
