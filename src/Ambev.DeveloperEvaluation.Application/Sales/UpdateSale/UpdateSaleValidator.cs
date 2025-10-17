5d4e6670cc8ff7bc2f4238c7e508f08a299f5a78
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-17 06:24:34 -0300

    feat(application): add UpdateSale validators

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateSaleValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.UpdateSale;
+
+/// <summary>
+/// Validator for UpdateSaleCommand that defines validation rules for the sale update command.
+/// </summary>
+public class UpdateSaleValidator : AbstractValidator<UpdateSaleCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of the UpdateSaleItemValidator with defined validation rules.
+    /// - CustomerId: Must be a valid Guid and cannot be empty
+    /// - CustomerName: Required (Length validation removed)
+    /// - BranchId: Must be a valid Guid and cannot be empty
+    /// - BranchName: Required (Length validation removed)
+    /// - Items: Cannot be empty and must have at least one valid item
+    /// </summary>
+    public UpdateSaleValidator()
+    {
+        RuleFor(sale => sale.Id)
+            .NotEmpty().WithMessage("Sale ID is required.");
+
+        RuleFor(sale => sale.CustomerId)
+            .NotEmpty().WithMessage("Customer ID is required.");
+
+        RuleFor(sale => sale.CustomerName)
+            .NotEmpty().WithMessage("Customer Name is required.");
+
+        RuleFor(sale => sale.BranchId)
+            .NotEmpty().WithMessage("Branch ID is required.");
+
+        RuleFor(sale => sale.BranchName)
+            .NotEmpty().WithMessage("Branch Name is required.");
+
+        RuleFor(sale => sale.Items)
+            .NotEmpty().WithMessage("Sale must contain at least one item.");
+
+        RuleForEach(sale => sale.Items).SetValidator(new UpdateSaleItemValidator());
+    }
+}
