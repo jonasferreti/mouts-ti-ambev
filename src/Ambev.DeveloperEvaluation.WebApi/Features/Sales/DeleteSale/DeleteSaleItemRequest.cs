1e752e57cb99f81d334622a7acacb2e82ac3017c
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-16 22:12:47 -0300

    feat(api): add DeleteSale request

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/DeleteSale/DeleteSaleItemRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
+
+/// <summary>
+/// Request model for deleting a sale item
+/// </summary>
+public class DeleteSaleItemRequest
+{
+    /// <summary>
+    /// The unique identifier of the sale
+    /// </summary>
+    public Guid SaleId { get; set; }
+
+    /// <summary>
+    /// The unique identifier of the sale item to delete
+    /// </summary>
+    public Guid ItemId { get; set; }
+}
+
