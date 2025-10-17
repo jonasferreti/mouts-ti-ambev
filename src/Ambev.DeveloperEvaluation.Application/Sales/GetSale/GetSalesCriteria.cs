0fd2c609cd26b282b855c323d64fe868ad07c7e4
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-17 20:15:27 -0300

    feat(application): add GetSale and GetSales queries

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSalesCriteria.cs
@@ -0,0 +1,25 @@
+﻿
+using Ambev.DeveloperEvaluation.Domain.Shared;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+/// <summary>
+/// Groups all filtering and ordering criteria for Sale queries.
+/// </summary>
+public record GetSalesCriteria
+{
+    /// <summary>Sale sort by field</summary>
+    public SaleSortField? SortField { get; init; }
+
+    /// <summary>Sale sort by field direction</summary>
+    public SortDirection? SortDirection { get; init; }
+
+    /// <summary>Filter by CustomerName</summary>
+    public string? CustomerName { get; init; }
+
+    /// <summary>Filter by BranchName</summary>
+    public string? BranchName { get; init; }
+
+    /// <summary>Filter by ProductName</summary>
+    public string? ProductName { get; init; }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSalesHandler.cs
@@ -0,0 +1,47 @@
+﻿using Ambev.DeveloperEvaluation.Application.Common;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Shared;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+/// <summary>
+/// Handler for the GetSalesQuery. Responsible for fetching a paginated list of Sale Aggregate Roots.
+/// </summary>
+public class GetSalesHandler : IRequestHandler<GetSalesQuery, PaginatedList<GetSaleResult>>
+{
+    private readonly ISaleRepository _saleRepository;
+    private readonly IMapper _mapper;
+
+    public GetSalesHandler(ISaleRepository repository, IMapper mapper)
+    {
+        _saleRepository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<GetSaleResult>> Handle(GetSalesQuery query, CancellationToken cancellationToken)
+    {
+        var validator = new GetSalesValidator();
+        var validationResult = await validator.ValidateAsync(query, cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var (sales, count) = await _saleRepository.GetPaginatedAsync(
+            query.PageNumber,
+            query.PageSize,
+            _mapper.Map<SaleSearchCriteria>(query.Criteria),
+            cancellationToken);
+
+        var result = _mapper.Map<List<GetSaleResult>>(sales);
+
+        return new PaginatedList<GetSaleResult>(
+            result,
+            count,
+            query.PageNumber,
+            query.PageSize
+        );
+    }
+}
