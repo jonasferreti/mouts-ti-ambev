978cb56f983f744991863c0ae0ba775b07b2c38c
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-16 22:12:47 -0300

    feat(api): add DeleteSale request

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/DeleteSale/DeleteSaleItemRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
+
+/// <summary>
+/// Validator for DeleteSaleItemRequest
+/// </summary>
+public class DeleteSaleItemRequestValidator : AbstractValidator<DeleteSaleItemRequest>
+{
+    /// <summary>
+    /// Initializes validation rules for DeleteSaleItemRequest
+    /// </summary>
+    public DeleteSaleItemRequestValidator()
+    {
+        RuleFor(x => x.SaleId)
+            .NotEmpty()
+            .WithMessage("Sale ID is required");
+
+        RuleFor(x => x.ItemId)
+            .NotEmpty()
+            .WithMessage("Item ID is required");
+    }
+}
+
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/DeleteSale/DeleteSaleRequestProfile.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
+
+/// <summary>
+/// Profile for mapping DeleteSale feature requests to commands
+/// </summary>
+public class DeleteSaleRequestProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for DeleteSale feature
+    /// </summary>
+    public DeleteSaleRequestProfile()
+    {
+        CreateMap<DeleteSaleRequest, DeleteSaleCommand>();
+        CreateMap<DeleteSaleItemRequest, DeleteSaleItemCommand>();
+
+    }
+}
