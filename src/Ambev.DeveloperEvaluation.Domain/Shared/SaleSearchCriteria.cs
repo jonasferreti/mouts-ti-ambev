7dc557db199668d2d130d8f48dc108863d665021
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-17 20:17:36 -0300

    feat(domain): add sale shared models

---
+++ src/Ambev.DeveloperEvaluation.Domain/Shared/SaleSearchCriteria.cs
@@ -0,0 +1,22 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Shared;
+
+/// <summary>
+/// Groups all filtering and ordering criteria for Sales.
+/// </summary>
+public record SaleSearchCriteria
+{
+    /// <summary>Sort by field</summary>
+    public SaleSortField? SortField { get; init; }
+
+    /// <summary>Sort by field direction</summary>
+    public SortDirection? SortDirection { get; set; }
+
+    /// <summary>Filter by Customer Name.</summary>
+    public string? CustomerName { get; init; }
+
+    /// <summary>Filter by Branch Name.</summary>
+    public string? BranchName { get; init; }
+
+    /// <summary>Filter by Product Name.</summary>
+    public string? ProductName { get; init; }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Shared/SortDirection.cs
@@ -0,0 +1,7 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Shared;
+
+public enum SortDirection
+{
+    Ascending,
+    Descending
+}
