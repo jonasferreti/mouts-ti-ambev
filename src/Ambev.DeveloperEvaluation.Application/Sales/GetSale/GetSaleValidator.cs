0d5c1cd9c6a393cf585ea54cc94d691370e8c792
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-17 20:16:24 -0300

    feat(application): add GetSale and GetSales validators

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+public class GetSaleValidator : AbstractValidator<GetSaleQuery>
+{
+    public GetSaleValidator()
+    {
+        RuleFor(item => item.Id)
+            .NotEmpty().WithMessage("Sale ID is required.");
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSalesValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+public class GetSalesValidator : AbstractValidator<GetSalesQuery>
+{
+    private const int MinPageSize = 1;
+    private const int MaxPageSize = 100;
+
+    public GetSalesValidator()
+    {
+        RuleFor(request => request.PageNumber)
+            .GreaterThan(0).WithMessage("The page number (_page) must be a positive integer.");
+
+        RuleFor(request => request.PageSize)
+            .InclusiveBetween(MinPageSize, MaxPageSize)
+            .WithMessage($"The page size (_size) must be between {MinPageSize} and {MaxPageSize}.");
+    }
+}
