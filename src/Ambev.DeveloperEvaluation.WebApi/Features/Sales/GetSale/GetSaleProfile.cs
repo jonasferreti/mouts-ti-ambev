69829255466c9d0b8944a3805f89d0b96d32d8d4
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-17 20:18:46 -0300

    feat(api): add GetSale profile

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleProfile.cs
@@ -0,0 +1,32 @@
+﻿using Ambev.DeveloperEvaluation.Application.Common;
+using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.WebApi.Helpers;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+
+/// <summary>
+/// AutoMapper profile for mapping between Application Layer DTOs and API Layer DTOs.
+/// </summary>
+public class GetSaleProfile : Profile
+{
+    public GetSaleProfile()
+    {
+        CreateMap<GetSaleRequest, GetSaleQuery>();
+
+        CreateMap<GetSalesRequest, GetSalesQuery>()
+            .ForMember(dest => dest.Criteria, opt => opt.MapFrom(src => new GetSalesCriteria
+            {
+                SortField = MappingHelpers.ConvertToSaleSortField(src.SortField),
+                SortDirection = MappingHelpers.ConvertToSortDirection(src.SortDirection),
+                CustomerName = src.CustomerName,
+                BranchName = src.BranchName,
+                ProductName = src.ProductName
+            }));
+
+        // application -> api
+        CreateMap<GetSaleItemResult, GetSaleItemResponse>();
+        CreateMap<GetSaleResult, GetSaleResponse>();
+    }
+}
