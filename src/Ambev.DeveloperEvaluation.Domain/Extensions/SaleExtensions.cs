e297a14b003d17e329eae4d4a67062cce2854665
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 08:46:11 -0300

    feat(domain): add events creation methods to SaleExtensions

---
+++ src/Ambev.DeveloperEvaluation.Domain/Extensions/SaleExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Events;
+
+namespace Ambev.DeveloperEvaluation.Domain.Extensions;
+
+public static class SaleExtensions
+{
+    public static SaleCreatedEvent SaleCreatedEvent(this Sale sale)
+    {
+        return new SaleCreatedEvent(
+            sale.Id,
+            sale.Customer.Value,
+            sale.Branch.Value,
+            sale.TotalAmount.Value
+        );
+    }
+
+    public static SaleCancelledEvent SaleCancelledEvent(this Sale sale)
+    {
+        return new SaleCancelledEvent(sale.Id);
+    }
+
+    public static SaleItemCancelledEvent SaleItemCancelledEvent(this Sale sale, Guid itemId)
+    {
+        return new SaleItemCancelledEvent(sale.Id, itemId);
+    }
+}
