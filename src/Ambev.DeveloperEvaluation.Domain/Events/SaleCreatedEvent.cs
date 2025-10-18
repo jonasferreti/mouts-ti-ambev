89ca2cc38203da99a1311e68f107acf1a7fb65b7
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 07:40:53 -0300

    feat(domain): add SaleCreatedEvent

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+
+/// <summary>
+/// Domain Event that is dispatched when a new Sale is successfully created and persisted.
+/// </summary>
+/// <param name="SaleId">The unique identifier of the created Sale.</param>
+/// <param name="CustomerId">The external identity of the Customer who made the purchase.</param>
+/// <param name="BranchId">The external identity of the Branch where the sale occurred.</param>
+/// <param name="TotalAmount">The final calculated amount of the sale.</param>
+public record SaleCreatedEvent(
+    Guid SaleId, Guid CustomerId,
+    Guid BranchId, decimal TotalAmount)
+{
+    public DateTime DateOccurred { get; } = DateTime.UtcNow;
+}
