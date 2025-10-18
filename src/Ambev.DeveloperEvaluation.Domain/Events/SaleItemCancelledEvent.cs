1ae0b87458f57e714d914f79fdeee693a81393ed
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 08:46:30 -0300

    feat(domain): add SaleItemCancelledEvent

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleItemCancelledEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+/// <summary>
+/// Domain Event that is dispatched when a new Sale Item is cancelled
+/// </summary>
+/// <param name="SaleId">The unique identifier of the Sale.</param>
+/// <param name="ItemId">The unique identifier of the cancelled Sale Item.</param>
+public record SaleItemCancelledEvent(Guid SaleId, Guid ItemId)
+{
+    public DateTime DateOccurred { get; } = DateTime.UtcNow;
+}
