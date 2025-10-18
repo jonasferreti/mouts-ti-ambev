a2762367c579c3c330049a640479e31ccab710e8
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 08:48:45 -0300

    feat(application): emit CancelSaleItemEvent

--- src/Ambev.DeveloperEvaluation.Application/Sales/CancelSale/CancelSaleItemHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CancelSale/CancelSaleItemHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Exceptions;
+using Ambev.DeveloperEvaluation.Domain.Extensions;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using MediatR;
+using Rebus.Bus;
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.CancelSale;
 
@@ -11,15 +13,16 @@ namespace Ambev.DeveloperEvaluation.Application.Sales.CancelSale;
 public class CancelSaleItemHandler : IRequestHandler<CancelSaleItemCommand>
 {
     private readonly ISaleRepository _saleRepository;
+    private readonly IBus _bus;
 
-    public CancelSaleItemHandler(ISaleRepository saleRepository)
+    public CancelSaleItemHandler(ISaleRepository saleRepository, IBus bus)
     {
         _saleRepository = saleRepository;
+        _bus = bus;
     }
 
     public async Task Handle(CancelSaleItemCommand command, CancellationToken cancellationToken)
     {
-        // 1. Load the Aggregate Root using the SaleId
         var sale = await _saleRepository.GetByIdForUpdateAsync(command.SaleId, cancellationToken)
             ?? throw new NotFoundException($"Sale with ID {command.SaleId} not found.");
 
@@ -28,6 +31,15 @@ public class CancelSaleItemHandler : IRequestHandler<CancelSaleItemCommand>
 
         sale.CancelItem(command.ItemId);
 
-        await _saleRepository.UpdateAsync(sale);
+        await _saleRepository.UpdateAsync(sale, cancellationToken);
+
+        var saleItemCancelledEvent = sale.SaleItemCancelledEvent(item.Id);
+        await _bus.Send(saleItemCancelledEvent);
+
+        if (sale.IsCancelled)
+        {
+            var saleCancelledEvent = sale.SaleCancelledEvent();
+            await _bus.Send(saleCancelledEvent);
+        }
     }
 }
