829c75ed319406e15d019aed150a5734d831c933
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 12:40:49 -0300

    feat(domain): add SaleDeleted and SaldeItemDeleted events

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleDeletedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+/// <summary>
+/// Domain Event that is dispatched when a new Sale is deleted
+/// </summary>
+/// <param name="SaleId">The unique identifier of the deleted Sale.</param>
+public record SaleDeletedEvent(Guid SaleId) 
+{
+    public DateTime DateOccurred { get; } = DateTime.UtcNow;
+}
