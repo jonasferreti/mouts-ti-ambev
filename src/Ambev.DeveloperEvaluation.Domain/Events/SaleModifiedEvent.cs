4c344e6f7e5ec952e7abb5f6456bd9ef68b954e6
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 08:57:27 -0300

    feat(domain): add SaleModifiedEvent

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleModifiedEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+/// <summary>
+/// Domain Event that is dispatched when a UpdateSale is successfully executed
+/// </summary>
+/// <param name="SaleId">The unique identifier of the updated Sale.</param>
+/// <param name="CustomerId">The external identity of the Customer who made the purchase.</param>
+/// <param name="BranchId">The external identity of the Branch where the sale occurred.</param>
+/// <param name="TotalAmount">The final calculated amount of the sale.</param>
+public record SaleModifiedEvent(
+    Guid SaleId, Guid CustomerId,
+    Guid BranchId, decimal TotalAmount)
+{
+    public DateTime DateOccurred { get; } = DateTime.UtcNow;
+}
