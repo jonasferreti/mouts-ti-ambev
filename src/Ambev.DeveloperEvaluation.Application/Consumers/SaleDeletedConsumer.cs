560b22bbfaae3d1d6a8d9b629f22da7bdffd8a81
Jonas Ferreti <157378249+jonasferreti@users.noreply.github.com>
2025-10-18 12:41:24 -0300

    feat(application): add SaleDeleted and SaleItemDeleted events consumers

---
+++ src/Ambev.DeveloperEvaluation.Application/Consumers/SaleDeletedConsumer.cs
@@ -0,0 +1,35 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Events;
+using Microsoft.Extensions.Logging;
+using Rebus.Handlers;
+
+namespace Ambev.DeveloperEvaluation.Application.Consumers;
+
+/// <summary>
+/// Rebus message handler responsible for consuming the SaleDeletedEvent
+/// </summary>
+public class SaleDeletedConsumer : IHandleMessages<SaleDeletedEvent>
+{
+    private readonly ILogger<SaleDeletedConsumer> _logger;
+
+    public SaleDeletedConsumer(ILogger<SaleDeletedConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Handles the received SaleDeletedEvent, logs the key information, and completes the message.
+    /// </summary>
+    /// <param name="message">The SaleDeletedEvent record containing sale details.</param>
+    /// <returns>A completed task, signaling Rebus that the message was processed successfully.</returns>
+    public Task Handle(SaleDeletedEvent message)
+    {
+        _logger.LogInformation(
+            "EVENT: Sale {SaleId} was deleted on {DateOccurred}",
+            message.SaleId,
+            message.DateOccurred
+        );
+
+        return Task.CompletedTask;
+    }
+}
+
---
+++ src/Ambev.DeveloperEvaluation.Application/Consumers/SaleItemDeletedConsumer.cs
@@ -0,0 +1,36 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Events;
+using Microsoft.Extensions.Logging;
+using Rebus.Handlers;
+
+namespace Ambev.DeveloperEvaluation.Application.Consumers;
+
+
+/// <summary>
+/// Rebus message handler responsible for consuming the SaleItemDeletedEvent
+/// </summary>
+public class SaleItemDeletedConsumer : IHandleMessages<SaleItemDeletedEvent>
+{
+    private readonly ILogger<SaleItemDeletedConsumer> _logger;
+
+    public SaleItemDeletedConsumer(ILogger<SaleItemDeletedConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Handles the received SaleItemDeletedEvent, logs the key information, and completes the message.
+    /// </summary>
+    /// <param name="message">The SaleItemDeletedEvent record containing sale details.</param>
+    /// <returns>A completed task, signaling Rebus that the message was processed successfully.</returns>
+    public Task Handle(SaleItemDeletedEvent message)
+    {
+        _logger.LogInformation(
+            "EVENT: SaleItem {ItemId} for Sale {SaleId} was deleted on {DateOccurred}",
+            message.ItemId,
+            message.SaleId,
+            message.DateOccurred
+        );
+
+        return Task.CompletedTask;
+    }
+}
